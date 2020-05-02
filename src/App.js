eeed94422329ffaafeb4adc13262287db13aa0e6
shinshin86 <beagles1986@gmail.com>
2020-05-02 18:53:10 +0900

    Create sample

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [ isSupportBatteryManager, setSupportBatteryManager] = useState(false);
+  const [ batteryCharging, setBatteryCharging ] = useState('');
+  const [ batteryLevel, setBatteryLevel] = useState('');
+  const [ batteryChargingTime, setBatteryChargingTime] = useState('');
+  const [ batteryDischargingTime, setBatteryDischargingTime] = useState('');
+
+  useEffect(() => {
+    const getBattery  = async () => {
+      if(window.navigator.getBattery) {
+        setSupportBatteryManager(true);
+
+        const battery = await window.navigator.getBattery()
+
+        setBatteryCharging(battery.charging ? 'Yes' : 'No');
+        setBatteryLevel(battery.level * 100 + "%");
+        setBatteryChargingTime(battery.chargingTime + " seconds");
+        setBatteryDischargingTime(battery.dischargingTime + " seconds")
+      } else {
+        setSupportBatteryManager(false);
+      }
+    }
+
+    getBattery();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isSupportBatteryManager ? (
+        <>
+              <p>Battery Charging: {batteryCharging}</p>
+              <p>Battery Level: {batteryLevel}</p>
+              <p>Battery Charging Time: {batteryChargingTime}</p>
+              <p>Battery Discharging Time: {batteryDischargingTime}</p>
+        </>
+      ) : (
+        <p>This browser is not support battery manager</p>
+      )}
+
     </div>
   );
 }
